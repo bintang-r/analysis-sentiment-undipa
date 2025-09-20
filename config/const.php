b62fa94cf55f21644628411b93a27740331a0147
M. Bintang Ramli. <90763575+bintang-r@users.noreply.github.com>
2025-09-20 12:17:29 +0800

    add: const variable logo

--- config/const.php
+++ config/const.php
@@ -5,4 +5,10 @@ return [
         'admin',
         'user',
     ],
+
+    "logo" => [
+        "light" => env("LOGO_LIGHT", 'ryoogenmedia/logo/LOGO 1.svg'),
+        "transparent" => env("LOGO_TRANSPARENT", 'ryoogenmedia/logo/LOGO 2.svg'),
+        "dark" => env('LOGO_DARK', 'ryoogenmedia/logo/LOGO 3.svg'),
+    ]
 ];
