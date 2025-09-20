873cea95d93b28dbc112fbf00038cf3b861702e7
M. Bintang Ramli. <90763575+bintang-r@users.noreply.github.com>
2025-09-20 12:58:32 +0800

    chore: app name env

--- config/const.php
+++ config/const.php
@@ -11,5 +11,7 @@ return [
         "light" => env("LOGO_LIGHT", 'ryoogenmedia/logo/LOGO 1.svg'),
         "transparent" => env("LOGO_TRANSPARENT", 'ryoogenmedia/logo/LOGO 2.svg'),
         "dark" => env('LOGO_DARK', 'ryoogenmedia/logo/LOGO 3.svg'),
-    ]
+    ],
+
+    'app_name' => env('APP_NAME', 'Ryoogen Pungawa Media'),
 ];
