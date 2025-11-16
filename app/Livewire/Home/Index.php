e6a5659eaca9e68622e7efd8cfc4b22fbc76d5b3
M. Bintang Ramli. <90763575+bintang-r@users.noreply.github.com>
2025-11-16 21:53:29 +0800

    little change

--- app/Livewire/Home/Index.php
+++ app/Livewire/Home/Index.php
@@ -2,12 +2,25 @@
 
 namespace App\Livewire\Home;
 
+use App\Models\User;
 use Livewire\Component;
 
 class Index extends Component
 {
+    public function getLoginHistories()
+    {
+        $user = User::query();
+
+        $query = $user->whereNotNull('last_login_time_232187')
+            ->orderBy('last_login_time_232187', 'DESC');
+
+        return $query->limit(20)->get();
+    }
+
     public function render()
     {
-        return view('livewire.home.index');
+        return view('livewire.home.index', [
+            'login_history' => $this->getLoginHistories(),
+        ]);
     }
 }
--- resources/views/livewire/home/index.blade.php
+++ resources/views/livewire/home/index.blade.php
@@ -4,4 +4,66 @@
     <x-slot name="pagePretitle">Ringkasan aplikasi anda berada disini.</x-slot>
 
     <x-slot name="pageTitle">Beranda</x-slot>
+
+    <div class="card mt-4" wire:poll.30000ms>
+        <h4 class="card-header">Riwayat Login Pengguna</h4>
+        <div class="card-body">
+            <div class="row">
+                @forelse ($login_history as $login)
+                    <div class="col col-md-4 col-xl-3 mb-2">
+                        <div>
+                            <div class="d-flex">
+                                <div class="mt-1 ms-1">
+                                    <img style="width: 53px; height: 53px; object-fit:cover; border-radius: 10px"
+                                        src="{{ $login->avatarUrl() }}" alt="avatar">
+                                </div>
+
+                                <div class="ms-2">
+                                    <div class="header font-weight-bold">
+                                        @if (isset($login->account_holder))
+                                            <small><b>{{ $login->account_holder->name_232187 ?? '-' }}</b></small>
+                                        @else
+                                            <small><b>{{ $login->username_232187 ?? '-' }}</b></small>
+                                        @endif
+
+                                        @if (is_online($login->id))
+                                            <span class="badge bg-success ms-1"></span>
+                                        @else
+                                            <small class="badge bg-secondary ms-1"
+                                                title="{{ $login->last_seen_time }}"></small>
+                                        @endif
+                                    </div>
+
+                                    <div class="subheader mb-1">
+                                        {{ \Carbon\Carbon::parse($login->last_login_time)->diffForHumans() ?? '-' }}
+                                    </div>
+
+                                    <div class="subheader mb-1">
+                                        <span @class([
+                                            'badge',
+                                            'bg-green-lt' => $login->role == 'admin',
+                                            'bg-blue-lt' => $login->role == 'guru',
+                                            'bg-yellow-lt' => $login->role == 'siswa',
+                                        ])>{{ $login->role }}</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                @empty
+                    <div class="empty">
+                        <div class="empty-icon">
+                            <i class="las la-clock" style="font-size:50px"></i>
+                        </div>
+
+                        <p class="empty-title">Data Riwayat Login Belum Ada.</p>
+
+                        <p class="empty-subtitle text-muted">
+                            Data riwayat login akan muncul dengan sendirinya, ketika akun pengguna sedang login.
+                        </p>
+                    </div>
+                @endforelse
+            </div>
+        </div>
+    </div>
 </div>
